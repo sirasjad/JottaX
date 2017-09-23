31ca0161b09975141d6d552f797717e858a168ec
Sirajuddin Asjad <sirasjad@gmail.com>
2017-09-24 01:08:50 +0200

    Converted UI to .py

--- login.py
+++ login.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'login.ui'
-#
-# Created by: PyQt5 UI code generator 5.5.1
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
